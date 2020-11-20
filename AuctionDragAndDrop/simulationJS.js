1b1b037ed023d31dc37af87a8cd03714e41b3295
Chirag Shilwant <48115585+Chirag-Shilwant@users.noreply.github.com>
2020-11-20 22:36:23 +0530

    Update simulationJS.js

--- AuctionDragAndDrop/simulationJS.js
+++ AuctionDragAndDrop/simulationJS.js
@@ -1,10 +1,11 @@
-var map1 = new Map([["task1" , "b1"], ["task2" , "b1"],["task3" , "b1"], ["task4" , "b1"], ["task5" , "b2"], ["task6" , "b2"], ["task7" , "b2"], ["task8" , "b3"], ["task9" , "b3"], ["task10" , "b3"], ["task11" , "b3"], ["task11a", "b7"], ["task11b", "b7"], ["task12", "b4"], ["task13", "b4"],["task14", "b2"], ["task15", "b5"], ["task16", "b5"], ["task17", "b5"], ["task18", "b5"], ["task19", "b6"], ["task20", "b6"], ["task21", "b6"], ["task22", "b8"], ["task23", "A1"], ["task24", "A2"],  ["task25", "A4"], ["task26", "A5"], ["task27", "A3"]]); 
+alertify.alert("1. Drag attributes, methods, etc from list on the LHS and drop it to its respective box in the class").set('resizable',true).resizeTo('50%',700).setHeader('Instructions');
+var map1 = new Map([["task1" , "b1"], ["task2" , "b1"],["task3" , "b1"], ["task4" , "b1"], ["task5" , "b2"], ["task6" , "b2"], ["task7" , "b2"], ["task8" , "b3"], ["task9" , "b3"], ["task10" , "b3"], ["task11" , "b3"], ["task11a", "b7"], ["task11b", "b7"], ["task11c", "b9"], ["task11d", "b10"], ["task11e", "b9"], ["task11f", "b10"], ["task12", "b4"], ["task13", "b4"],["task14", "b2"], ["task15", "b5"], ["task16", "b5"], ["task17", "b5"], ["task18", "b5"], ["task19", "b6"], ["task20", "b6"], ["task21", "b6"], ["task22", "b8"], ["task22a", "b9a"], ["task22b", "b10a"], ["task22c", "b9a"], ["task22d", "b10a"], ["task23", "A1"], ["task24", "A2"],  ["task25", "A4"], ["task26", "A5"], ["task27", "A3"]]); 
 
 var dropTarget = document.querySelector(".wrapper");
 var draggables = document.querySelectorAll(".task");
-
-console.log(draggables)
-console.log(dropTarget)
+var count = 0;
+console.log(draggables.length)
+//console.log(dropTarget)
 
 for(let i = 0; i < draggables.length; i++) {
   draggables[i].addEventListener("dragstart", function (ev) {
@@ -22,21 +23,29 @@ dropTarget.addEventListener('drop', function(ev) {
   let target = ev.target;
   let droppable  = target.classList.contains('box');
   let srcId = ev.dataTransfer.getData("srcId");
-  console.log(target.id)
-  console.log(map1.get(srcId))
+  //console.log(target.id)
+  //console.log(map1.get(srcId))
   if(droppable)
   {
 	  if (map1.get(srcId) == target.id) {
 	    ev.target.appendChild(document.getElementById(srcId));
+	    alertify.set('notifier','position', 'top-right');
+	   
+	    alertify.success('Good going 🤝');
+	   
+	    count++;
 	  }
 	  else
 	  {
-	    alert("Wrong Answer")
+	    alertify.set('notifier','position', 'top-right');
+	    alertify.error("Hard Luck 😕. Please try again")
+	   
           }
   }
   else
   {
-  	alert("Drop the item in the rectangle only")
+  	 alertify.set('notifier','position', 'top-right');
+	 alertify.error('Please Drop elements in Rectangular Boxes Only!!');
   }
-          
+   
 });
